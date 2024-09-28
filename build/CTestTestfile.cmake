d7c7e897745ad525de265739e5e348f002dc3612
Ivan <ivanbolbin@gmail.com>
2024-09-28 17:09:07 +0700

    7 commit

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -4,3 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(tests "tests")
+set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "D:/projects/programming/Lab_2/MyCode/CMakeLists.txt;66;add_test;D:/projects/programming/Lab_2/MyCode/CMakeLists.txt;0;")
---
+++ tests.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_MODULE test_version
+
+#include "main.cpp"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_version)
+
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
+    std::string l = "1.0.1.0";
+    std::string r = "2.0.1.0";
+    std::string r1 = "1.0.2.0";
+    BOOST_CHECK(compare(l, r));
+    BOOST_CHECK(compare(r, l));
+    BOOST_CHECK(compare(l, r1));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
