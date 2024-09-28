9e17ce4772cc2e1ff7a8e930884e535e1bcbc3d1
Ivan <ivanbolbin@gmail.com>
2024-09-28 17:46:19 +0700

    12 commit

--- tests.cpp
+++ tests.cpp
@@ -11,9 +11,9 @@ BOOST_AUTO_TEST_CASE(test_valid_version)
     std::string l = "1.0.1.0";
     std::string r = "2.0.1.0";
     std::string r1 = "1.0.2.0";
-    BOOST_CHECK(compare(l, r));
-    BOOST_CHECK(compare(r, l));
-    BOOST_CHECK(compare(l, r1));
+    BOOST_CHECK(compare(l, r) == 0);
+    BOOST_CHECK(compare(r, l) == 1);
+    BOOST_CHECK(compare(l, r1) == 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
